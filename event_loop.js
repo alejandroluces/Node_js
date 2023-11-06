a23fa45a20ef25436d162fa366c1f41413d078b1
Alejandro Luces González <aluces@polytex.local>
2023-11-06 20:55:05 -0300

    AddInfo_EventLoop

--- event_loop.js
+++ event_loop.js
@@ -6,3 +6,22 @@
 
 // console.log("third");
 
+const http = require('http');
+
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.write("WELCOME TO SERVER");
+        res.end();
+    }
+    // task
+    // blocking Code
+    if(req.url === '/about'){
+        for(let i=0;i<100000;i++){
+            console.log(Math.random()*i);
+        }
+        return res.end("Not page");
+    }
+    res.end("Not found");
+})
+server.listen(3000)
+console.log(`Listening on port 3000`);
